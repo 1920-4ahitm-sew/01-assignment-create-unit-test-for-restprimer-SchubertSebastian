6a232175ad582ad94c04b3e4d971f288bce1e51e
Sebastian Schubert <39259189+C0rruptdeX@users.noreply.github.com>
2019-10-10 16:50:20 +0200

    added fetchTime in 'TimeServerResourceIT'

--- src/test/java/at/htl/restprimer/rest/TimeServerResourceIT.java
+++ src/test/java/at/htl/restprimer/rest/TimeServerResourceIT.java
@@ -1,10 +1,18 @@
 package at.htl.restprimer.rest;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertTrue;
+
 
 public class TimeServerResourceIT {
     private Client client;
@@ -14,5 +22,13 @@ public class TimeServerResourceIT {
     public void initCLient() {
         this.client = ClientBuilder.newClient();
         this.tut = this.client.target("http://localhost:8080/restprimer/rs/time")
+    }
+
+    @Test
+    public void fetchTime() {
+        Response response = this.tut.request(MediaType.TEXT_PLAIN).get();
+        assertThat(response.getStatus(), is(200));
+        String payload = response.readEntity(String.class);
+        assertTrue(payload.startsWith("Time: "));
     }
 }
