2415c28b1b20a2db66ce85b22e8055372297e1d6
Sebastian Schubert <39259189+C0rruptdeX@users.noreply.github.com>
2019-10-10 16:43:39 +0200

    added initClient in 'TimeServerResourceIT'

--- src/test/java/at/htl/restprimer/rest/TimeServerResourceIT.java
+++ src/test/java/at/htl/restprimer/rest/TimeServerResourceIT.java
@@ -1,4 +1,18 @@
 package at.htl.restprimer.rest;
 
+import org.junit.Before;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
 public class TimeServerResourceIT {
+    private Client client;
+    private WebTarget tut;
+
+    @Before
+    public void initCLient() {
+        this.client = ClientBuilder.newClient();
+        this.tut = this.client.target("http://localhost:8080/restprimer/rs/time")
+    }
 }
